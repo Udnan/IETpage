54d76eca23dc9f1b544206712eb336dda7519444
Gopal <gopalkurup@gmail.com>
2016-12-26 14:02:35 +0530

    IET logo added to login page

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if (isset($_POST['btn-login'])) {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Coding Cage - Login & Registration System</title>
+<title>IET:Institution of Engineering and Technology, CET Chapter</title>
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen"> 
 <link rel="stylesheet" href="style.css" type="text/css" />
@@ -46,10 +46,10 @@ if (isset($_POST['btn-login'])) {
 
 	<div class="container">
      
-        
+       <div class="img-cont"><img class="img" src="IET.png" alt="IET logo"></div> 
        <form class="form-signin" method="post" id="login-form">
       
-        <h2 class="form-signin-heading">Sign In.</h2><hr />
+        <h2 class="form-signin-heading">Sign In</h2><hr />
         
         <?php
 		if(isset($msg)){
